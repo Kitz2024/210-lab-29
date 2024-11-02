2ee53f021f333d7950e1b5baebb03dbe5d424407
Kitz2024 <kit.pollinger@gmail.com>
2024-11-02 14:07:29 -0700

    Lab 29 Commit 1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Kit Pollinger
+// 210 - Lab - 29 | Pseudocode and Mockup
+
+#include <iostream>
+#include <map>
+#include <array>
+#include <list>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+}
